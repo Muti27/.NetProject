62547812f7c3cee5a22c5ff82923dd5196b04789
Muti <wolf8625@gmail.com>
2025-09-26 01:13:16 +0800

    新增InMemory Database

---
+++ Mvc/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mvc.Models;
+
+namespace Mvc.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
