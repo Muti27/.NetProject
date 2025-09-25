fea0d152fc69ec7185788e59797c122991b55988
Muti <wolf8625@gmail.com>
2025-09-26 01:11:34 +0800

    新增swagger套件

---
+++ Mvc/Controllers/ProjectsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllProjects()
+    {
+        var projects = new[]
+        {
+            new { Id = 1, Name = "First Project", Description = "Demo" },
+            new { Id = 2, Name = "Second Project", Description = "Demo 2" }
+        };
+        return Ok(projects);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProjectById(int id)
+    {
+        return Ok(new { Id = id, Name = $"Project {id}", Description = "Detail" });
+    }
+}
